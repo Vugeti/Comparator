1b8a08fc41e6e5c878ce6b5b9dd576d573e430af
Vugeti <vugeti@gmail.com>
2021-12-27 15:57:53 +0200

    new EngineCapacityComparator

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,4 +1,5 @@
 import java.awt.geom.Point2D;
+import java.util.Comparator;
 import java.util.List;
 
 public class Car implements Comparable<Car> {
--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,5 +1,5 @@
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.testng.annotations.Test;
 
 import java.util.*;
@@ -9,6 +9,13 @@ public class CarTest {
     Car car1 = new Car(2015, 2000);
     Car car2 = new Car(2019, 1400);
     Car car3 = new Car(2017, 1600);
+@BeforeEach
+    public void setUp(){
+    car1 = new Car(2015, 2000);
+    car2 = new Car(2019, 1400);
+    car3 = new Car(2017, 1600);
+}
+
 
     @Test
     public void shouldSortAsc() {
@@ -33,7 +40,7 @@ public class CarTest {
     @Test
     void testTreeSetAndSort() {
 
-        TreeSet<Car> setSort = new TreeSet<>(Comparator.comparing(car -> car.getEngineCapacity()));
+        TreeSet<Car> setSort = new TreeSet<Car>(new EngineCapacityComparator());
         setSort.add(car3);
         setSort.add(car1);
         setSort.add(car2);
